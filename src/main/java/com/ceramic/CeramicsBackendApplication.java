c3ca3ab1435e35981384292961db1f0265865b76
Thế Anh Oxy <139890192+TheAnhOxy@users.noreply.github.com>
2026-08-24 16:16:55 +0700

    chore: init project

---
+++ src/main/java/com/ceramic/CeramicsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ceramic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeramicsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CeramicsBackendApplication.class, args);
+    }
+
+}
