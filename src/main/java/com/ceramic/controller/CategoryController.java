f8b4ffc7b57781fe1938c5e7693778522eb6aae6
Thế Anh Oxy <139890192+TheAnhOxy@users.noreply.github.com>
2026-08-24 23:30:38 +0700

    feat: update category controller

--- src/main/java/com/ceramic/controller/CategoryController.java
+++ src/main/java/com/ceramic/controller/CategoryController.java
@@ -1,4 +1,13 @@
 package com.ceramic.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CategoryController {
+
+    @GetMapping("hi")
+    public String hi(){
+        return "ahihi";
+    }
 }
